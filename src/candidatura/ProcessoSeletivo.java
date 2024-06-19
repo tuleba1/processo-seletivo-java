851ca2b096c466fdf23f34cfc527e4e7532b6876
Túlio Dantas <tuliopragana@hotmail.com>
2024-06-19 19:55:51 -0300

    alteração de detalhes no código

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,8 +1,7 @@
 package candidatura;
 
-import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Scanner;
+
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
